c13e42c045b6d0af3931991afbcb891f4c07cf59
Caique Andrade <caiquembandrade@gmail.com>
2018-01-27 00:29:54 -0200

    Atualização para 304-api-final

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,3 +1,5 @@
+const ObjectID = require('mongodb').ObjectID;
+
 // Função para listar todos os meus usuários que estão no banco de dados na coleção 'usuários'
 exports.listar = (req, res) => {
     req.db.collection('usuarios').find().toArray((err, result) => {
@@ -12,6 +14,39 @@ exports.listar = (req, res) => {
 
 // Função para testar o POST na minha rota '/api/usuarios/usuario'
 exports.criar = (req, res) => {
-    console.log('Processando requisição de um novo usuario.');
+    req.db.collection('usuarios').insert(req.body, (err, result) => {
+        if(err){
+            res.send(err);
+        }
+        else{
+            res.sendStatus(201);
+        }
+    });
+}
+
+exports.atualizar = (req, res) => {
+    let id = req.params.idDaUrl;
+
+    req.db.collection('usuarios').update({"_id": ObjectID(id)}, req.body, (err, result) => {
+        if(err){
+            res.send(err);
+        }
+        else{
+            res.sendStatus(200);
+        }
+    });
+}
+
+exports.deletar = (req, res) => {
+    let id = req.params.idDaUrl;
+
+    req.db.collection('usuarios').remove({"_id": ObjectID(id)}, (err, result) => {
+        if(err){
+            res.send(err);
+        }
+        else{
+            res.sendStatus(200);
+        }
+    });
 }
 
--- index.js
+++ index.js
@@ -13,13 +13,15 @@ const app = express();
 app.use(bodyParser.json());
 
 // Conecta a nossa api ao banco de dados
-app.use(expressMongoDb('mongodb://localhost/estacaohack-turma2'));
+app.use(expressMongoDb('mongodb://localhost/api-final-2'));
 
 // Inicializando o servidor
 app.listen(3000, () => {
     console.log('Servidor ligado. Acesse em http://localhost:3000');
 });
 
-// Endpoints
+// Endpoints / Rotas
 app.get('/api/usuarios', usuariosController.listar);
 app.post('/api/usuarios/usuario', usuariosController.criar);
+app.put('/api/usuarios/usuario/:idDaUrl', usuariosController.atualizar);
+app.delete('/api/usuarios/usuario/:idDaUrl', usuariosController.deletar);
